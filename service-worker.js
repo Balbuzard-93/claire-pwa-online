6111fa335f4de16c405eab4389fd10de66720e05
Balbuzard-93 <thibault.lambert@hotmail.com>
2025-05-07 18:24:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = 'claire-static-cache-v53'; // <<<< VERSION INCRÉMENTÉE
+const CACHE_NAME = 'claire-static-cache-v54'; // <<<< VERSION INCRÉMENTÉE
 
 const APP_SHELL_URLS = [
     '/', '/index.html', '/style.css', '/app.js',
